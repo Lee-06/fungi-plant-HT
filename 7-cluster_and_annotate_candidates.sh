e3bc7130afc2dcca9ee9b1f631ff2ee1596597a4
Lee M. <75045401+Lee-06@users.noreply.github.com>
2025-01-30 17:47:52 +0100

    Update 7-cluster_and_annotate_candidates.sh

--- 7-cluster_and_annotate_candidates.sh
+++ 7-cluster_and_annotate_candidates.sh
@@ -7,7 +7,7 @@ emapper.py -i hgt_clusters.fasta \
            --output hgt_annotations \
            --cpu 8 \
            --tax_scope 2759 \
-           --dmnd_db /media/lee/bigdata/eggnog_database_files/eggnog_proteins.dmnd \
+           --dmnd_db [TOREPLACE]/eggnog_database_files/eggnog_proteins.dmnd \
            -m diamond \
-           --data_dir /media/lee/bigdata/eggnog_database_files \
+           --data_dir [TOREPLACE]/eggnog_database_files \
            --translate
