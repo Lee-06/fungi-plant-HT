4307e8a207a08b3243df672d00992c7741a49fd2
Lee M. <75045401+Lee-06@users.noreply.github.com>
2026-01-30 11:40:26 +0100

    Enhance script with error handling and path updates
    Added error handling for missing EggNOG database path and updated EggNOG Mapper command to use the provided path for database files.

--- 7-cluster_and_annotate_candidates.sh
+++ 7-cluster_and_annotate_candidates.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+if [[ -z "$1" ]]; then
+    echo "Error: Missing EggNOG database path."
+    echo "Usage: $0 <path_to_eggnog_data_dir>"
+    exit 1
+fi
+
+EGGNOG_DATA_DIR="$1"
+
+# Run CD-HIT
 cd-hit -i hgt_candidates.fasta -o hgt_clusters.fasta -c 0.9 -n 5 -d 0 -T 8 -M 16000
 
-#eggnog is required for the following step
+# Run EggNOG Mapper using the provided path
 emapper.py -i hgt_clusters.fasta \
            --output hgt_annotations \
            --cpu 8 \
            --tax_scope 2759 \
-           --dmnd_db [TOREPLACE]/eggnog_database_files/eggnog_proteins.dmnd \
+           --dmnd_db "${EGGNOG_DATA_DIR}/eggnog_proteins.dmnd" \
            -m diamond \
-           --data_dir [TOREPLACE]/eggnog_database_files \
+           --data_dir "${EGGNOG_DATA_DIR}" \
            --translate
